bbdbf1d8b56d4a133b6538adfc1a78c31934baaa
omar-medeuov <omarmedeuov@gmail.com>
2025-07-12 13:26:41 +0500

    ticket urls added

--- orders/urls.py
+++ orders/urls.py
@@ -1,13 +1,18 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
 
-from .views import OrderListView, OrderDetailView, UserRegistrationView
+from .views import OrderListView, OrderDetailView, UserRegistrationView, TicketListView, TicketDetailView
 
 urlpatterns = [
 
     path("register/", UserRegistrationView.as_view(), name="user_registration"),
     path("api-token-auth/", obtain_auth_token),
+
+    path("tickets/", TicketListView.as_view(), name="ticket_list"),
+    path("tickets/<int:pk>/", TicketDetailView.as_view(), name="ticket_detail"),
     path("<int:pk>/", OrderDetailView.as_view(), name="order_detail"),
     path("", OrderListView.as_view(), name="order_list"),
+
+
 
 ]
